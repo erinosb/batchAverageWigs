d74eb8df21118af83d8f0ee9fee59144c74c9d4c
erinosb <erinosb@gmail.com>
2016-06-13 16:56:44 -0400

    added readme content

--- batchAverageWigs.sh
+++ batchAverageWigs.sh
@@ -1,58 +1,64 @@
 #!/bin/bash
 
-##################################################################################
-#batch_average_wigs.sh
+###################################################################################
+## batchAverageWigs
 #
-# Creative Commons CC0 1.0 License 2016, Erin Osborne Nishimura
+### Creative Commons CC0 1.0 License 2016, Erin Osborne Nishimura
 #
-#VERSION:
+### VERSION:
 #       1
 #
-#AUTHOR:
+### AUTHOR:
 #       Erin Osborne Nishimura
 #
-#DATE:
+### DATE:
 #       June 13, 2016         
 #
-#PURPOSE:
+### PURPOSE:
 #       A very basic wrapper to merge together replicates of wig files by averaging across conditions in batch.
 #
 #
-#USAGE:
-#       bash 02_average_bws.sh metafile.txt chromfile.txt
+### USAGE:
+#       ./batchAverageWigs [options] metafile.txt chromfile.txt
+#            -u | --uppath       URL address to provide to UCSC genome browser specifying the ftp/http address of the final .bw file
+#            -w | --keepWigs     An option to retain the temporary NAME_avg.wig files. Default will delete these files and retain only .bw files
 #
 #
-#INPUT:
-#       1) metafile.txt --> a tab-delimited text file (.txt) that lists the replicate bw files and their conditions
+### INPUT:
+#       1) metafile.txt --> a tab-delimited text file (.txt) that lists the replicate .wig files and their conditions
 #               Example:
-#               ../03_OUTPUT/st225_test.bw	ABpl
-#               ../03_OUTPUT/st226_test.bw	ABpl
+#               ../03_OUTPUT/st225_test.wig	ABpl
+#               ../03_OUTPUT/st226_test.wig	ABpl
 #       2) chromfile.txt --> a chromosome length file
 #               Requires chromosome length file. downloaded from: http://hgdownload.cse.ucsc.edu/downloads.html
 #               Example, for ce10 c. elegans: http://hgdownload.cse.ucsc.edu/goldenPath/ce10/bigZips/ce10.chrom.sizes
 #       3) the .wig files listed in the metafile. These need to be real .wig files. See https://genome.ucsc.edu/goldenpath/help/wiggle.html
 #
 #
-#OUTPUT:
+### OUTPUT:
 #       NAME_avg.wig -- averaged .wig files
 #       NAME_avg.bw -- averaged bw files
 #       DATE_avg_bw_tracks.log -- a .txt file of custom track descriptions
 #     
 #
-#EXAMPLE:
+### EXAMPLE:
 #       bash 02_average_bws.sh fixed_metadata_test.txt /proj/dllab/Erin/ce10/from_ucsc/seq/chr_length_ce10.txt
 #
-#REQUIREMENTS:
+### REQUIREMENTS:
 #       Requires java-genomics-toolkit available from: https://github.com/timpalpant/java-genomics-toolkit
 #       Requires the ucsc genome utility wigToBigWig available: http://hgdownload.soe.ucsc.edu/admin/exe/
 #
 #
-#KNOWN BUGS:
+### KNOWN BUGS:
 #
-#FUTURE EXTENSION:
-#       -- Add an option to put the track info in the metadata file.
+### FUTURE EXTENSION:
+#       -- Add an option to put the track info in the metadata file
+#
 #
 ##################################################################################
+
+
+
 
 #Get options
 uppath=
@@ -61,7 +67,7 @@ for n in $@
 do
     case $n in
 
-        -u) shift;
+        -u | --uppath) shift;
         uppath=$1;
         shift;
         ;;
